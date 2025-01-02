657443f90f012aa660747728072a24937c002894
Sultana Akther <sultana.akther2805@gmail.com>
2025-01-02 02:05:00 +0000

    Save the works for script.js and script.test.js

--- script/tests/script.test.js
+++ script/tests/script.test.js
@@ -1,4 +1,4 @@
-const { script } = require("../script");
+const {game, newGame, showScore} = require("../script");
 
 beforeAll(() =>{
     let fs = require("fs");
@@ -9,20 +9,44 @@ beforeAll(() =>{
 });
 describe("game object contains correct keys", ()=>{
     test("score key exists", () => {
-        expect("score" in script).toBe(true);
+        expect("score" in game).toBe(true);
     });
     test("currentGame key exists", () => {
-        expect("currentGame" in script).toBe(true);
+        expect("currentGame" in game).toBe(true);
     });
     test("playerMoves key exists", () => {
-        expect("playerMoves" in script).toBe(true);
+        expect("playerMoves" in game).toBe(true);
     });
     test("choices key exists", () => {
-        expect("choices" in script).toBe(true);
+        expect("choices" in game).toBe(true);
     });
     test("choices contain correct ids", () => {
-        expect(script.choices).toEqual(["button1", "button2", "button3", "button4"]);
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+
+    });
+
+    describe("newGame works correctly", () => {
+        beforeAll(() => {
+            game.score = 42;
+            game.currentGame=["button1", "button2"];
+            game.playerMoves=["button1", "button2"];
+            document.getElementById("score").innerText="42";
+            newGame();
+        });
+        test("should set game score to zero", () => {
+            expect(game.score).toEqual(0);
+        });
+        test("should clear the computer sequence array",  () => {
+            expect(game.currentGame.length).toBe(0);
+        });
+        test("should clear the player moves array", () => {
+            expect(game.playerMoves.length).toBe(0);
+        });
+        test("should display 0 for the element with id of score", () => {
+            expect(document.getElementById("score").innerText).toEqual(0);
+        });
     });
     
-});
+
 
