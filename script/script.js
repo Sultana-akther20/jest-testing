26769e55e00672eec8fc01d9df23caa2efb85c58
Sultana Akther <sultana.akther2805@gmail.com>
2025-01-05 20:46:08 +0000

    Save the testing files for script.test.js and script.js

--- script/script.js
+++ script/script.js
@@ -9,6 +9,17 @@ function newGame(){
     game.score=0;
     game.currentGame=[];
     game.playerMoves=[];
+    for (let circle of document.getElementsByClassName("circle")){
+        if (circle.getAttribute("data-listener") !== "true"){
+            circle.addEventListener("click", (e) =>{
+                let move = e.target.getAttribute("id");
+                lightOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
     showScore();
     addTurn();
 }
@@ -21,5 +32,36 @@ function addTurn(){
 function showScore(){
     document.getElementById("score").innerText = game.score;
 }
+function lightOn(circ){
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+function showTurns(){
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length){
+            clearInterval(turns);
+        }
+    }, 800);
+}
 
-module.exports = {game, newGame, showScore};
+function playerTurn(){
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]){
+        if (game.currentGame.length==game.playerMoves.length){
+            game.score++;
+            showScore();
+            addTurn();
+        }
+    }else{
+        alert("Wrong Move!");
+        newGame();
+    }
+    }
+
+
+module.exports = {game, newGame, showScore, addTurn, lightOn, showTurns, playerTurn};
--- script/tests/script.test.js
+++ script/tests/script.test.js
@@ -1,4 +1,7 @@
-const {game, newGame, showScore} = require("../script");
+const {game, newGame, showScore, addTurn, lightOn, showTurns, playerTurn} = require("../script");
+
+jest.spyOn(window, "alert").mockImplementation(() =>{});
+
 
 beforeAll(() =>{
     let fs = require("fs");
@@ -45,6 +48,51 @@ describe("game object contains correct keys", ()=>{
         });
         test("should display 0 for the element with id of score", () => {
             expect(document.getElementById("score").innerText).toEqual(0);
+        });
+        test("expect data-listener to be true", () => {
+            const elements=document.getElementsByClassName("circle");
+            for (let element of elements){
+                expect(element.getAttribute("data-listener")).toEqual("true");
+            }
+        });
+
+    });
+
+    describe("gameplay works correctly", () =>{
+        beforeEach (()=> {
+            game.score = 0;
+            game.currentGame = [];
+            game.playerMoves = [];
+            addTurn();
+        });
+        afterEach(() => {
+            game.score = 0;
+            game.currentGame = [];
+            game.playerMoves = [];
+        });
+        test("addTurn adds a new turn to the game", () => {
+            addTurn();
+            expect(game.currentGame.length).toBe(2);
+        });
+        test("should add correct class to light up the buttons", () => {
+            let button = document.getElementById(game.currentGame[0]);
+            lightOn(game.currentGame[0]);
+            expect(button.classList).toContain("light");
+        });
+        test("showTurns should update game.turnNumber", () => {
+            game.turnNumber = 42;
+            showTurns();
+            expect(game.turnNumber).toBe(0);
+        });
+        test("should increment the score if the turn is correct", () => {
+            game.playerMoves.push(game.currentGame[0]);
+            playerTurn();
+            expect(game.score).toBe(1);
+        });
+        test("should call an alert if the move is wrong", () => {
+            game.playerMoves.push("wrong");
+            playerTurn();
+            expect(window.alert).toBeCalledWith("Wrong Move!");
         });
     });
     
